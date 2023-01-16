402a790769c4cb080913521ee722568202506998
john.09 <john.09@kakaocorp.com>
2023-01-16 20:13:26 +0900

    init

---
+++ src/main/kotlin/com/nexters/pimo/PimoApplication.kt
@@ -0,0 +1,11 @@
+package com.nexters.pimo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PimoApplication
+
+fun main(args: Array<String>) {
+    runApplication<PimoApplication>(*args)
+}
---
+++ src/test/kotlin/com/nexters/pimo/PimoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nexters.pimo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PimoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
